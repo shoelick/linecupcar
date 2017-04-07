f4201bd635322cdf3cd8dc55c3bddb6a0cb48d4c
shoelick <git@megascops.net>
2017-04-07 11:09:08 -0400

    Actual cleanup time

--- keil/Objects/system_mk64f12.d
+++
@@ -1,9 +0,0 @@
-.\objects\system_mk64f12.o: RTE\Device\MK64FN1M0xxx12\system_MK64F12.c
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K60_DFP\1.4.0\Device\Include\MK64F12.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cm4.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cminstr.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\cmsis_armcc.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmfunc.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmsimd.h
-.\objects\system_mk64f12.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K60_DFP\1.4.0\Device\Include\system_MK64F12.h
