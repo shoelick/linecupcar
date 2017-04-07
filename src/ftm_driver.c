8815207b86e1f299de28fb84812666052e7366b0
shoelick <git@megascops.net>
2017-04-07 14:54:01 -0400

    It's counting

--- src/ftm_driver.c
+++ src/ftm_driver.c
@@ -49,42 +49,45 @@ int ftm_init(ftm_driver *drv, int num){
 		drv->regs->MODE |= FTM_MODE_WPDIS_MASK;
 		
 		// Set output to '1' on init
-		drv->regs->OUTINIT |= FTM_OUTINIT_CH0OI_MASK;
+		//drv->regs->OUTINIT |= FTM_OUTINIT_CH0OI_MASK;
 		
 		// Initialize the CNT to 0 before writing to MOD
 		drv->regs->CNT = 0;
 		
 		// Set the Counter Initial Value to 0
-		drv->regs->CNTIN = 0;
+		//drv->regs->CNTIN = 0;
 		
 		// Set the period (~10us)
 		drv->regs->MOD = 100;
 		
-		// 50% duty
-		drv->regs->CONTROLS[0].CnV = 50;
-		
 		// Set edge-aligned mode
-		drv->regs->CONTROLS[0].CnSC |= FTM_CnSC_MSB_MASK;
+		//drv->regs->CONTROLS[0].CnSC |= FTM_CnSC_MSB_MASK;
 		
 		// Enable High-true pulses
 		// ELSB = 1, ELSA = 0
-		drv->regs->CONTROLS[0].CnSC |= FTM_CnSC_ELSB_MASK;
-		drv->regs->CONTROLS[0].CnSC &= ~(FTM_CnSC_ELSA_MASK);
+		//drv->regs->CONTROLS[0].CnSC |= FTM_CnSC_ELSB_MASK;
+		//drv->regs->CONTROLS[0].CnSC &= ~(FTM_CnSC_ELSA_MASK);
+		
+		// 50% duty
+		//drv->regs->CONTROLS[0].CnV = 50;
 		
 		// Enable hardware trigger from FTM2
-		drv->regs->EXTTRIG |= FTM_EXTTRIG_INITTRIGEN_MASK;
+		//drv->regs->EXTTRIG |= FTM_EXTTRIG_INITTRIGEN_MASK;
 		
 		// Don't enable interrupts yet (disable)
-		drv->regs->SC &= ~(FTM_SC_TOIE_MASK);
+		//drv->regs->SC &= ~(FTM_SC_TOIE_MASK);
 		
 		// No prescalar, system clock
-		drv->regs->SC = FTM_SC_PS(0)| FTM_SC_CLKS(1);
+		drv->regs->SC = FTM_SC_PS(7)| FTM_SC_CLKS(1);
 	}
 	
 	return retval;
 }
 
 void ftm_enable_int(ftm_driver *drv) {
+	
+	// Enable FTM2 interrupts (camera)
+	drv->regs->SC |= FTM_SC_TOIE_MASK;
 	
 	// Set up interrupt
 	NVIC_EnableIRQ(drv->irqn);
